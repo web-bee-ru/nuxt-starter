752928e5916bbc225f0fb2f0586206d810abd5c9
Denis Karabaza <denis.karabaza@gmail.com>
2020-08-06 12:39:04 +0300

    Fix doc for webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-// @DOC: This file is only used by IDE, and does not participate in build.
-//       Go to "Settings -> Language & Frameworks -> Javascript -> Webpack" and choose this file.
+// @DOC: Go to "Settings -> Language & Frameworks -> Javascript -> Webpack" and choose this file.
+//       This file is only used by IDE, and does not participate in build.
 // @REFERENCE: https://github.com/nuxt/nuxt.js/issues/2936
 
 const path = require('path');
