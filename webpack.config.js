6763e9803b0e762107d1c687595b707a57a40bce
Konstantin Barabanov <criitch@yandex.ru>
2021-05-13 12:25:11 +0300

    fix: поправил работу ide с импортом в стилях

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,8 @@ module.exports = {
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      '~': path.resolve(__dirname, 'src'),
+      '~@': path.resolve(__dirname, 'src'),
     },
   },
 };
